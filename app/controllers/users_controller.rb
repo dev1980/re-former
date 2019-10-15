39671a448e1135e3f927c905c15142d34d095b8a
Miguel Prada <mapra99@gmail.com>
2019-10-15 12:12:04 -0500

    fixed typo in form, and completed 'create' action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:users).permit(:username,:email,:password)
   end
 end
